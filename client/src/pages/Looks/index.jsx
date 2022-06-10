efa070c52e8b42b777b857085b005b6b76697cb7
Wanderson Chaves <wandersonscpibr@gmail.com>
2022-06-10 11:17:14 -0300

    updatedHome

--- client/src/pages/Looks/index.jsx
+++ client/src/pages/Looks/index.jsx
@@ -29,131 +29,69 @@ export function Looks() {
 
   // Return the look if a document was retrieved from Prismic
   if (look) {
-    try {
-      return (
-        <div className="bg-white h-screen">
-          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <header className="flex justify-between">
-              <Link to="/">
-                <ImArrowLeft2 color="#000" />
-              </Link>
-              <Link to="/">
-                <ImHome color="#000" />
-              </Link>
-            </header>
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-              <div key={look[0].id} className="group relative">
-                <div className="w-full h-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                  <img
-                    src={look[0].data.image_look.url}
-                    alt={look[0].data.image_look.alt}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
-                </div>
-              </div>
-
-              <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto h-screen">
-                {look.map((unitLook) => (
-                  <div
-                    key={unitLook.id}
-                    onClick={handleUnitLookId}
-                    className="group relative"
-                  >
-                    <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                      <img
-                        src={unitLook.data.image_look.url}
-                        alt={unitLook.data.image_look.alt}
-                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                      />
-                    </div>
-                    <div className="mt-4 flex justify-between">
-                      <div>
-                        <h3 className="text-sm text-gray-700">
-                          <Link to={unitLook.uid} onClick={handleUnitLookId}>
-                            <span
-                              aria-hidden="true"
-                              className="absolute inset-0"
-                            />
-                          </Link>
-                        </h3>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <footer className="flex justify-end">
-              <button className=" flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                <Link to="/">
-                  <ImHome color="#000" />
-                </Link>
-                ESCOLHER ESTE LOOK
-              </button>
-            </footer>
-          </div>
-        </div>
-      );
-    } catch (error) {
-      console.log(error);
-    }
-    <div className="bg-white h-screen">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <header className="flex justify-between">
-          <Link to="/">
-            <ImArrowLeft2 color="#000" />
-          </Link>
-          <Link to="/">
-            <ImHome color="#000" />
-          </Link>
-        </header>
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-          <div key={look[0].id} className="group relative">
-            <div className="w-full h-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-              <img
-                src={look[0].data.image_look.url}
-                alt={look[0].data.image_look.alt}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-          </div>
-
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto h-screen">
-            {look.map((unitLook) => (
-              <div
-                key={unitLook.id}
-                onClick={handleUnitLookId}
-                className="group relative"
-              >
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                  <img
-                    src={unitLook.data.image_look.url}
-                    alt={unitLook.data.image_look.alt}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <Link to={unitLook.uid} onClick={handleUnitLookId}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                      </Link>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <footer className="flex justify-end">
-          <button className=" flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+    return (
+      <div className="bg-white h-screen">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <header className="flex justify-between">
+            <Link to="/">
+              <ImArrowLeft2 color="#000" />
+            </Link>
             <Link to="/">
               <ImHome color="#000" />
             </Link>
-            ESCOLHER ESTE LOOK
-          </button>
-        </footer>
+          </header>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
+            <div key={look[0].id} className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                <img
+                  src={look[0].data.image_look.url}
+                  alt={look[0].data.image_look.alt}
+                  className="w-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto h-80">
+              {look.map((unitLook) => (
+                <div
+                  key={unitLook.id}
+                  onClick={handleUnitLookId}
+                  className="group relative"
+                >
+                  <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                    <img
+                      src={unitLook.data.image_look.url}
+                      alt={unitLook.data.image_look.alt}
+                      className="w-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <Link to={unitLook.uid} onClick={handleUnitLookId}>
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0"
+                          />
+                        </Link>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <footer className="flex justify-end">
+            <button className=" flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              <Link to="/">
+                <ImHome color="#000" />
+              </Link>
+              ESCOLHER ESTE LOOK
+            </button>
+          </footer>
+        </div>
       </div>
-    </div>;
+    );
   } else if (notFound) {
     return <NotFound />;
   }
