24c621c0345e3f13ebb56babbf91a9bc4c67e3fe
Wanderson Chaves <wandersonscpibr@gmail.com>
2022-06-10 00:46:53 -0300

    add new layout for all screens

--- client/src/components/LookItem/index.jsx
+++ client/src/components/LookItem/index.jsx
@@ -59,71 +59,73 @@ export function LookItem() {
     return (
       <>
         <Layout wrapperClassName="look" menuDoc={menu}>
-        {/* <SliceZone slices={look.data.look_content} components={components} /> */}
-        <div className="bg-white max-h-full">
-          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <header className="flex justify-between">
-              <Link to="/looks">
-                <ImArrowLeft2 color="#000" />
-              </Link>
-              <Link to="/">
-                <ImHome color="#000" />
-              </Link>
-            </header>
+          {/* <SliceZone slices={look.data.look_content} components={components} /> */}
+          <div className="bg-white max-h-full">
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+              <header className="flex justify-between">
+                <Link to="/looks">
+                  <ImArrowLeft2 color="#000" />
+                </Link>
+                <Link to="/">
+                  <ImHome color="#000" />
+                </Link>
+              </header>
 
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-              <div key={look.id} className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                  <img
-                    src={look.data.image_look.url}
-                    alt={look.data.image_look.alt}
-                    className="w-full object-center object-cover lg:w-full lg:h-full"
-                  />
+              <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
+                <div key={look.id} className="group relative">
+                  <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                    <img
+                      src={look.data.image_look.url}
+                      alt={look.data.image_look.alt}
+                      className="w-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-rows-3 xl:gap-x-8">
-                <div className="group relative">
-                  <div className="w-full h-full flex justify-center items-center min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                    <h1>
-                      Olha só, este look que você escolheu tem disponível na
-                      loja do Deck
-                    </h1>
+                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-rows-3 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="w-full h-full flex justify-center items-center min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                      <h1>
+                        Olha só, este look que você escolheu tem disponível na
+                        loja do Deck
+                      </h1>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-              <div key={look.id} className="group relative">
-                <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                  <img
-                    src={look.data.qr_code.url}
-                    alt={look.data.image_look.alt}
-                    className="w-full object-center object-cover lg:w-full lg:h-full"
-                  />
-                </div>
-              </div>
-
-              <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-rows-3 xl:gap-x-8">
-                <div className="text-center w-full h-full flex justify-center items-center min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                  <h1>
-                    Você pode entrar em contato diretamente com eles via
-                    Whatsapp no QRCode ao lado
-                  </h1>
+              <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
+                <div key={look.id} className="group relative">
+                  <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                    {look.data.qr_code && (
+                      <img
+                        src={look.data.qr_code.url}
+                        alt={look.data.qr_code.alt}
+                        className="w-full object-center object-cover lg:w-full lg:h-full"
+                      />
+                    )}
+                  </div>
                 </div>
 
-                <div className="">
-                  <div className="w-full h-full flex justify-center items-end min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                      Button
-                    </button>
+                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-rows-3 xl:gap-x-8">
+                  <div className="text-center w-full h-full flex justify-center items-center min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                    <h1>
+                      Você pode entrar em contato diretamente com eles via
+                      Whatsapp no QRCode ao lado
+                    </h1>
+                  </div>
+
+                  <div className="">
+                    <div className="w-full h-full flex justify-center items-end min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75  lg:aspect-none">
+                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Button
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </Layout>
       </>
     );
